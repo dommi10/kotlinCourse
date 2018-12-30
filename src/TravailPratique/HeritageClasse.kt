84559a1e05e4a960ffb71aa08ab2f324859f8cc4
dommi10 <domsbuhendwa2@gmail.com>
2018-12-30 19:45:22 +0200

    Mon tp BIGURWA BUHENDWA

--- src/TravailPratique/HeritageClasse.kt
+++ src/TravailPratique/HeritageClasse.kt
@@ -1,5 +1,35 @@
 package TravailPratique
 
-fun main(args:Array<String>){
+open class Personne(){
+    var nom:String?=null
+    var age:Int=0
+    constructor(nom:String,age:Int):this(){
+        this.nom=nom
+        this.age=age
+    }
+    init {
 
+    }
+}
+class Agent():Personne(){
+    var SalaireBase:Float?=0f
+    var Grade:String?=null
+    var Affectation:String?=null
+    constructor(SalaireBase:Float,Grade:String,Affectation:String):this(){
+        this.SalaireBase=SalaireBase
+        this.Grade=Grade
+        this.Affectation=Affectation
+    }
+}
+class Pere():Personne(){
+    var ville:String?=null
+    constructor(ville:String):this(){
+        this.ville=ville
+    }
+}
+fun main(args:Array<String>){
+    val personne = Personne("dom's",21)
+    val agent = Agent(10f,"Secretaire","Bureau")
+    val pere = Pere("Goma")
+    print("La personne ${agent.SalaireBase}")
 }
